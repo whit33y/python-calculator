7e3d773020046226890c11d4aa1d94373824b477
whit33y <troppout@gmail.com>
2022-03-22 20:23:18 +0100

    2 versions, second much cleaner

--- calculator.py
+++ calculator.py
@@ -1,13 +1,12 @@
 result = 0
 chosen_operation_check = False
 while chosen_operation_check == False:
-    chosen_operation = input('a=add, s=subtract, m=multiply, d=divide, e=exit, r=reset: ')
+    chosen_operation = input('a=add, s=subtract, m=multiply, d=divide, r=reset, e=exit   ')
     if chosen_operation == 'e':
         chosen_operation_check = True
     elif chosen_operation == 'a' or chosen_operation == 's' or chosen_operation =='m' or chosen_operation =='d':
         if result == 0:
             result = float(input('Give number: '))
-            print(result,'')
             chosen_number = float(input('Give number: '))
             if chosen_operation == 'a':
                 result = result + chosen_number
--- calculator2.py
+++ calculator2.py
@@ -1,28 +1,43 @@
-# def operation_and_proccess(operation,user_number,results):
-#         if operation == 'a':
-#             results+=user_number
-#             return results
-#         elif operation == 's':
-#             results-=user_number
-#             return  results
-#         elif operation == 'm':
-#             results*=user_number
-#             return results
-#         elif operation == 'r':
-#             results = 0
-#             return results
-#         else:
-#             return('I dont get it')           
-# result=0
-# chosen_operation_check = False
-# while chosen_operation_check == False:
-#     chosen_operation = input('a=add, s=subtract, m=multiply, e=exit, r=reset: ')
-#     if chosen_operation == 'e':
-#         chosen_operation_check = True
-#     elif chosen_operation == 'a':
-#         if result = 0:
-#             user_number = input
-#     elif chosen_operation == 's':
-#     elif chosen_operation == 'm':
-#     else:
-#         print('I dont get it')
+def add(x,y):
+    return x+y
+def subtract(x,y):
+    return x-y
+def multiply(x,y):
+    return x*y
+def divide(x,y):
+    return x/y
+result = 0
+while True:
+    choice = input('a-add s-subtract m-multiply d-divide ')
+    if choice in('a','s','m','d'):
+        if result == 0:
+            num1 = float(input('Enter number: '))
+            num2 = float(input('Enter number: '))
+            if choice == 'a':
+                result = add(num1, num2)
+                print(num1, "+", num2, "=", add(num1, num2))
+            elif choice == 's':
+                result = subtract(num1, num2)
+                print(num1, "-", num2, "=", subtract(num1, num2))
+            elif choice == 'm':
+                result = multiply(num1, num2)
+                print(num1, "*", num2, "=", multiply(num1, num2))
+            elif choice == 'd':
+                result = divide(num1, num2)
+                print(num1, "/", num2, "=", divide(num1, num2))
+        else:
+            num2 = float(input('Enter number: '))
+            if choice == 'a':
+                print(result, "+", num2, "=", add(result, num2))
+                result+=num2
+            elif choice == 's':
+                print(result, "-", num2, "=", subtract(result, num2))
+                result-=num2
+            elif choice == 'm':
+                print(result, "*", num2, "=", multiply(result, num2))
+                result*=num2
+            elif choice == 'd':
+                print(result, "/", num2, "=", divide(result, num2))
+                result/=num2 
+    else:
+        print('I dont know it')
