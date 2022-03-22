792e8a566f73f0a395f2ad8ae0a8558c4c2d4e7f
whit33y <troppout@gmail.com>
2022-03-22 19:23:13 +0100

    First making it as easiest as posible

--- calculator.py
+++ calculator.py
@@ -1,35 +1,44 @@
-def operation(operation,user_number,result):
-    if operation == 'a':
-        result+=user_number
-        return result
-    elif operation == 's':
-        result-=user_number
-        return result  
-    elif operation == 'm':
-        result = result * user_number
-        return result
-    else:
-        return('Wrong') 
 result = 0
 chosen_operation_check = False
 while chosen_operation_check == False:
-    chosen_operation = input('a=add, s=subtract, m=multiply, e=exit: ')
+    chosen_operation = input('a=add, s=subtract, m=multiply, d=divide, e=exit, r=reset: ')
     if chosen_operation == 'e':
         chosen_operation_check = True
-    else:
+    elif chosen_operation == 'a' or chosen_operation == 's' or chosen_operation =='m' or chosen_operation =='d':
         if result == 0:
-            result = int(input('Give number: '))
-            chosen_number = int(input('Give number: '))
-            print(operation(chosen_operation,chosen_number,result))
-            result+=chosen_number
-        else:
-            chosen_number = int(input('Give number: '))
-            print(operation(chosen_operation,chosen_number,result))
+            result = float(input('Give number: '))
+            print(result,'')
+            chosen_number = float(input('Give number: '))
             if chosen_operation == 'a':
-                result+=chosen_number 
+                result = result + chosen_number
+                print(result)
             elif chosen_operation == 's':
-                result-=chosen_number 
+                result = result - chosen_number
+                print(result)
             elif chosen_operation == 'm':
-                result = result*chosen_number 
+                result = result*chosen_number
+                print(result)
+            elif chosen_operation == 'd':
+                result = result/chosen_number
+                print(result) 
+        else:
+            chosen_number = float(input('Give number: '))
+            if chosen_operation == 'a':
+                result = result + chosen_number
+                print(result) 
+            elif chosen_operation == 's':
+                result = result - chosen_number
+                print(result)
+            elif chosen_operation == 'm':
+                result = result*chosen_number
+                print(result)
+            elif chosen_operation == 'd':
+                result = result/chosen_number
+                print(result) 
+    elif chosen_operation == 'r':
+        result = 0
+    else:
+        print('I dont get it')
+        chosen_operation==False
 
 
---
+++ calculator2.py
@@ -0,0 +1,28 @@
+# def operation_and_proccess(operation,user_number,results):
+#         if operation == 'a':
+#             results+=user_number
+#             return results
+#         elif operation == 's':
+#             results-=user_number
+#             return  results
+#         elif operation == 'm':
+#             results*=user_number
+#             return results
+#         elif operation == 'r':
+#             results = 0
+#             return results
+#         else:
+#             return('I dont get it')           
+# result=0
+# chosen_operation_check = False
+# while chosen_operation_check == False:
+#     chosen_operation = input('a=add, s=subtract, m=multiply, e=exit, r=reset: ')
+#     if chosen_operation == 'e':
+#         chosen_operation_check = True
+#     elif chosen_operation == 'a':
+#         if result = 0:
+#             user_number = input
+#     elif chosen_operation == 's':
+#     elif chosen_operation == 'm':
+#     else:
+#         print('I dont get it')
